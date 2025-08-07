9fbbc2be683d62145363123cfa96eb09355d2baa
rikuto <rikuto042700@icloud.com>
2025-08-07 18:31:38 +0900

    負け画面の装飾

--- src/feature/battleResult/LoseScreen.jsx
+++ src/feature/battleResult/LoseScreen.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import "./LoseScreen.css";
 function LoseScreen({ task, resetGame }) {
   return (
     <div className="lose-screen">
-      <h1>😢Mission Failed...😢</h1>
-      <p>あなたは{task}を完了できませんでした</p>
-      <p>次はもっと頑張りましょう！</p>
-      <button onClick={resetGame}>🏠 ホームに戻る</button>
+      <div className="lose-content">
+        <h1 className="lose-title">💀Mission Failed...💀</h1>
+        <div className="lose-details">
+          <p className="task-failed">
+            あなたは「{task}」を完了できませんでした
+          </p>
+          <p className="encouragement">次はもっと頑張りましょう！</p>
+        </div>
+        <button className="home-button" onClick={resetGame}>
+          🏠 ホームに戻る
+        </button>
+      </div>
     </div>
   );
 }

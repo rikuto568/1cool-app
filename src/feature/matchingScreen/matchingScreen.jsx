bb986fa1f924ac3f4b234281cdc85a3c5de519da
rikuto <rikuto042700@icloud.com>
2025-08-07 16:58:26 +0900

    matchingScreenの装飾

--- src/feature/matchingScreen/matchingScreen.jsx
+++ src/feature/matchingScreen/matchingScreen.jsx
@@ -1,15 +1,16 @@
 import LoadingSpinner from "./loadingSpinner";
 import React from "react";
+import "./matchingScreen.css";
 
 function MatchingScreen({ isMatching }) {
   return (
     <div className="matching-screen">
-      <h1>マッチング中...</h1>
+      <h1 className="matching-title">マッチング中...</h1>
       <div className="spinner">
         {isMatching && <LoadingSpinner />}
         {/* 上でローディンんぐ画面になってる */}
       </div>
-      <p>AIがタスク時間を計算中です...</p>
+      <p className="matching-description">AIがタスク時間を計算中です...</p>
     </div>
   );
 }
